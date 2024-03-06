f178d7ae6ab7c0b8f48a40a28c5a4e7308f3b20d
datafoia <datafoia@gmail.com>
2024-03-05 21:06:19 -0300

    Subindo transform para o github

---
+++ app/pipeline/transform.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from typing import List
+
+
+"""
+Função para transformar uma lista de dataframes em um dataframe
+"""
+
+def concat_dataframes(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+    return pd.concat(data_frame_list)
+
