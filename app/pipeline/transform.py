772c506073e183ceb9e7cc753af9f392529ab04e
datafoia <datafoia@gmail.com>
2024-03-05 21:29:55 -0300

    Test transform funcionando

--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -7,5 +7,5 @@ Função para transformar uma lista de dataframes em um dataframe
 """
 
 def concat_dataframes(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
-    return pd.concat(data_frame_list)
+    return pd.concat(data_frame_list, ignore_index = True)
 
