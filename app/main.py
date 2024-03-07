cbde67d537c52f6c2938853c026677c177e25e26
datafoia <datafoia@gmail.com>
2024-03-07 20:27:27 -0300

    Testar CI na nova branch

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from pipeline.extract import extract_data
 from pipeline.load import load_excel
 from pipeline.transform import concat_dataframes
 
-if __name__ == '__main__':
-    listas_de_data_frame = extract_data('data/input')
+if __name__ == "__main__":
+    listas_de_data_frame = extract_data("data/input")
     data_frame = concat_dataframes(listas_de_data_frame)
-    load_excel(data_frame, 'data/output', 'output')
+    print("CI OK!")
+    load_excel(data_frame, "data/output", "output")
